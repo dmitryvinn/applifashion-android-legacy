5f2ba48e2312a5894d400296f80c0f59e068e387
Dmitry Vinnik <dmitryvinn@users.noreply.github.com>
2022-06-05 23:39:12 -0700

    Adding Product Activity functionality

--- app/src/main/java/com/applitools/applifashion/main/CustomAdapter.java
+++ app/src/main/java/com/applitools/applifashion/main/CustomAdapter.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,16 +24,50 @@ public class CustomAdapter extends RecyclerView.Adapter {
     private final List<Shoe> shoes;
     private final Context context;
 
+    private static final int FOOTER_VIEW = 1;
+
+// Define a view holder for Footer view
+
+    public class FooterViewHolder extends RecyclerView.ViewHolder {
+        public FooterViewHolder(View itemView) {
+            super(itemView);
+            TextView linkTextView = (TextView) itemView.findViewById(R.id.about_us);
+//            linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+//            HyperLink = (TextView)findViewById(R.id.textView1);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
+
     public CustomAdapter(final Context context, final List<Shoe> shoes) {
         this.context = context;
         this.shoes = shoes;
     }
 
     @Override
-    public ShoeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v;
+        if (viewType == FOOTER_VIEW) {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.footer_layout, parent, false);
+
+            FooterViewHolder vh = new FooterViewHolder(v);
+
+//            StaggeredGridLayoutManager.LayoutParams layoutParams = (StaggeredGridLayoutManager.LayoutParams) vh.itemView.getLayoutParams();
+//            layoutParams.setFullSpan(true);
+            return vh;
+        } else {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_row_layout,
+                    parent, false);
+        }
         // infalte the item Layout
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_row_layout,
-                parent, false);
+
         // set the view's size, margins, paddings and layout parameters
         ShoeViewHolder vh = new ShoeViewHolder(v); // pass the view to View Holdercontext
         return vh;
@@ -41,35 +78,73 @@ public class CustomAdapter extends RecyclerView.Adapter {
     public void onBindViewHolder(RecyclerView.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         // set the data in items
 
-        final ShoeViewHolder shoeViewHolder = (ShoeViewHolder) holder;
-        // implement setOnClickListener event on item view.
-        final Shoe shoe = shoes.get(position);
-        shoeViewHolder.setCurrentPrice(shoe.getCurrentPrice());
-        shoeViewHolder.setShoeName(shoe.getName());
-        final Drawable shoeImage = context.getResources().getDrawable(shoe.getImageId());
-        shoeViewHolder.setShoeImage(shoeImage);
+        try {
+            if (holder instanceof ShoeViewHolder) {
+                final ShoeViewHolder shoeViewHolder = (ShoeViewHolder) holder;
+                // implement setOnClickListener event on item view.
+                final Shoe shoe = shoes.get(position);
+                shoeViewHolder.setCurrentPrice(shoe.getCurrentPrice());
+                shoeViewHolder.setShoeName(shoe.getName());
+                final Drawable shoeImage = context.getResources().getDrawable(shoe.getImageId());
+                shoeViewHolder.setShoeImage(shoeImage);
 
-        if (shoe.getOldPrice() == null) {
-            shoeViewHolder.hideDiscountInfo();
-        } else {
-            shoeViewHolder.setOldPrice(shoe.getOldPrice());
+                if (shoe.getOldPrice() == null) {
+                    shoeViewHolder.hideDiscountInfo();
+                } else {
+                    shoeViewHolder.setOldPrice(shoe.getOldPrice());
 
+                }
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // open another activity on item click
+                        Intent intent = new Intent(context, ProductActivity.class);
+                        intent.putExtra("shoe", shoe); // put image data in Intent
+                        context.startActivity(intent); // start Intent
+                    }
+                });
+            } else if (holder instanceof FooterViewHolder) {
+                FooterViewHolder vh = (FooterViewHolder) holder;
+
+                StaggeredGridLayoutManager.LayoutParams layoutParams = (StaggeredGridLayoutManager.LayoutParams) vh.itemView.getLayoutParams();
+                if ( layoutParams == null ) {
+                    layoutParams = new StaggeredGridLayoutManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                }
+                layoutParams.setFullSpan(true);
+                vh.itemView.setLayoutParams(layoutParams);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // open another activity on item click
-                Intent intent = new Intent(context, ProductActivity.class);
-                intent.putExtra("shoe", shoe); // put image data in Intent
-                context.startActivity(intent); // start Intent
-            }
-        });
     }
     @Override
     public int getItemCount() {
-        return shoes.size();
+        if (shoes == null) {
+            return 0;
+        }
+
+        if (shoes.size() == 0) {
+            //Return 1 here to show nothing
+            return 1;
+        }
+
+        // Add extra view to show the footer view
+        return shoes.size() + 1;
     }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == shoes.size()) {
+            // This is where we'll add footer.
+            return FOOTER_VIEW;
+        }
+
+        return super.getItemViewType(position);
+    }
+
     public class ShoeViewHolder extends RecyclerView.ViewHolder {
         // init the item view's
 
--- app/src/main/java/com/applitools/applifashion/main/MainActivity.java
+++ app/src/main/java/com/applitools/applifashion/main/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import java.util.List;
 
@@ -19,15 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+
+
         final List<Shoe> shoes = ShoesGenerator.getInstance().getShoes();
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         // set a GridLayoutManager with default vertical orientation and 2 number of columns
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),2);
+        StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(gridLayoutManager); // set LayoutManager to RecyclerView
         //  call the constructor of CustomAdapter to send the reference and data to Adapter
         CustomAdapter customAdapter = new CustomAdapter(MainActivity.this, shoes);
         recyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
+
     }
 
 }
